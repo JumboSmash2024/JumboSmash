007824a91c789a820de3d6b97c884e4e5b3922fb
JumboSmash <jumbosmash2024@gmail.com>
2024-05-16 13:21:40 -0400

    UI updates
    Pre styles, help text

--- web/src/Pages/HelpPage.php
+++ web/src/Pages/HelpPage.php
@@ -28,36 +28,7 @@ with the person that they mean. If you both decide to connect, then in the match
 you will see each others' personal emails to actually connect with each other.
 END
             ),
-            HTMLBuilder::element(
-                'p',
-                <<<END
-If you both decide to connect, anything you do after that is up to you - we are
-all adults here. A few reminders though:
-END
-            ),
         ];
-        $reminders = [
-            'You can only decide to connect ("smash") with people who have registered accounts.',
-            'As a result, you should keep checking back for new accounts that you want to connect with.',
-            'Matching just means you want to connect, it does not imply consent to anything! NO MEANS NO.',
-            'You will not receive a notification when you have a new match.',
-            'If you do end up doing anything (I leave what that might mean to your imagination, 😏), use protection.',
-            [
-                'If you have any problems with the interface or account, please contact ',
-                HTMLBuilder::element( 'code', 'jumbosmash2024@gmail.com' ),
-            ],
-            'Do not reuse passwords - I wrote this in under 24 hours and it should not be treated as secure!!!',
-            'Account registration emails may take a while to arrive, and will probably be in your spam folder.',
-        ];
-        $reminderList = HTMLBuilder::element(
-            'ul',
-            array_map(
-                static fn ( $rem ) => HTMLBuilder::element( 'li', $rem ),
-                $reminders
-            )
-        );
-        $elems[] = $reminderList;
-
         $email = HTMLBuilder::element( 'code', 'jumbosmash2024@gmail.com' );
         $regSteps = [
             [ 'Send an email to ', $email, ' from your TUFTS email and CC the personal email' ],
@@ -71,12 +42,42 @@ END
                 $regSteps
             )
         );
-        $elems[] = HTMLBuilder::element( 'hr' );
         $elems[] = HTMLBuilder::element(
             'p',
             'To register your account, do the following:'
         );
         $elems[] = $registration;
+        $elems[] = HTMLBuilder::element( 'hr' );
+
+        $elems[] =  HTMLBuilder::element(
+            'p',
+            <<<END
+If you both decide to connect, anything you do after that is up to you - we are
+all adults here. A few reminders though:
+END
+        );
+        $reminders = [
+            'You can only decide to connect ("smash") with people who have registered accounts.',
+            'As a result, you should keep checking back for new accounts that you want to connect with.',
+            'Matching just means you want to connect, it does not imply consent to anything! NO MEANS NO.',
+            'You will not receive a notification when you have a new match.',
+            'If you do end up doing anything (I leave what that might mean to your imagination, 😏), use protection.',
+            [
+                'If you have any problems with the interface or account, please contact ',
+                HTMLBuilder::element( 'code', 'jumbosmash2024@gmail.com' ),
+            ],
+            'Do not reuse passwords - I wrote this in under 24 hours and it should not be treated as secure!!!',
+            'Account verification is all manual, sorry if it is slow.',
+        ];
+        $reminderList = HTMLBuilder::element(
+            'ul',
+            array_map(
+                static fn ( $rem ) => HTMLBuilder::element( 'li', $rem ),
+                $reminders
+            )
+        );
+        $elems[] = $reminderList;
+
         return $elems;
     }
 }
