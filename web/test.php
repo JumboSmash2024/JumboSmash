231e4d3316bfe6944a81de0791e24d1ce6a52ce0
JumboSmash <jumbosmash2024@gmail.com>
2024-05-15 22:52:07 -0400

    Remove email testing

--- web/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/src/setup.php';
-
-\JumboSmash\Services\Mailer::sendMail(
-    \JumboSmash\Services\Mailer::SENDER_EMAIL,
-    'Test with Mailer service (subject)',
-    'Test with Mailer service (message)'
-);
